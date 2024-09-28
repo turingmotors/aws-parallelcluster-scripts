422a52441bb58d1e66070ee895710be1c5592c3b
Kazuki Matsuda <mats.kazuki@gmail.com>
2024-09-28 15:44:04 +0900

    Update agent-init.sh

--- coder/agent-init.sh
+++ coder/agent-init.sh
@@ -3,13 +3,29 @@
 CODER_DIR="/opt/coder"
 mkdir -m 777 ${CODER_DIR}
 
-echo "$1" > ${CODER_DIR}/init_script.sh
-chmod +x ${CODER_DIR}/init_script.sh
+echo "$1" > ${CODER_DIR}/agent_init.sh
+chmod +x ${CODER_DIR}/agent_init.sh
 
-CODER_AGENT_USER="${2:-ubuntu}"
-CODER_AGENT_CMD="nohup ${CODER_DIR}/init_script.sh >/dev/null 2>&1 &"
-sudo -u ${CODER_AGENT_USER} bash -c "${CODER_AGENT_CMD}"
+tee /etc/systemd/system/coder-agent.service > /dev/null << EOT
+[Unit]
+Description=Coder Agent
+After=network.target
 
-echo "@reboot /bin/bash -c \"${CODER_AGENT_CMD}\"" | sudo -u ${CODER_AGENT_USER} crontab -
+[Service]
+Type=simple
+User=ubuntu
+ExecStart=/bin/bash /opt/coder/agent_init.sh
+Restart=on-failure
+RestartSec=5
+StandardOutput=journal
+StandardError=journal
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+systemctl daemon-reload
+systemctl enable coder-agent.service
+systemctl start coder-agent.service
 
 exit 0
