b61dc0563a5111c30c159b14b1695272b319acbb
Kazuki Matsuda <mats.kazuki@gmail.com>
2024-10-01 15:34:08 +0900

    Update install.sh

--- fleet_status/install.sh
+++ fleet_status/install.sh
@@ -13,7 +13,7 @@ tee /etc/systemd/system/parallelcluster-compute-fleet-status.service > /dev/null
 [Unit]
 Description=Update Fleet Status on startup and shutdown
 After=network-online.target
-Before=network.target supervisord.service
+Before=network-online.target supervisord.service
 
 [Service]
 Type=oneshot
