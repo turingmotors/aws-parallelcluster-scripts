53fe2a0aa1176d130133526e097e04312525a0a0
Kazuki Matsuda <mats.kazuki@gmail.com>
2024-10-01 14:00:15 +0900

    Update install.sh

--- fleet_status/install.sh
+++ fleet_status/install.sh
@@ -3,7 +3,11 @@
 SCRIPT_URL="https://raw.githubusercontent.com/turingmotors/aws-parallelcluster-scripts/main/fleet_status/update_compute_fleet_status.sh"
 SCRIPT_PATH="/opt/parallelcluster/update_fleet_status.sh"
 
+# スクリプトを取得
 curl -o ${SCRIPT_PATH} ${SCRIPT_URL}
+
+# 実行権限を付与
+chmod +x ${SCRIPT_PATH}
 
 tee /etc/systemd/system/update_fleet_status_start.service > /dev/null << EOT
 [Unit]
