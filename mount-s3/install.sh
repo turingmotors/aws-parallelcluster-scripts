8f80f076a04fb808e825b1a5fe2d4c02dd800dcb
Kazuki Matsuda <mats.kazuki@gmail.com>
2025-01-08 02:44:52 +0900

    Update install.sh

--- mount-s3/install.sh
+++ mount-s3/install.sh
@@ -1,8 +1,19 @@
-#! /bin/bash
+#!/bin/bash
+
+BUCKET_NAME=$1
+TARGET_DIRECTORY=$2
 
 # Install mount-s3 command for Ubuntu 22.04
-apt-get update
-apt-get install libfuse2 -y
+if ! command -v mount-s3 &> /dev/null
+then
+  apt-get update
+  apt-get install libfuse2 -y
+  wget -O /tmp/mount-s3.deb https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.deb
+  apt-get install /tmp/mount-s3.deb -y
+else
+  echo "mount-s3 is already installed."
+fi
 
-wget -O /tmp/mount-s3.deb https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.deb
-apt-get install /tmp/mount-s3.deb -y
+mkdir -p ${TARGET_DIRECTORY}
+chmod 777 ${TARGET_DIRECTORY}
+sudo -u ubuntu mount-s3 ${BUCKET_NAME} ${TARGET_DIRECTORY}
