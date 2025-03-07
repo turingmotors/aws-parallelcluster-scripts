94ad0538a6b65ce02d2915afb07723eb8ef00f6a
Kazuki Matsuda <mats.kazuki@gmail.com>
2025-03-07 12:07:11 +0900

    Update apt-install.sh

--- core/apt-install.sh
+++ core/apt-install.sh
@@ -4,5 +4,5 @@ set -exo pipefail
 
 PACKAGES=$@
 
-apt-get -y update
-apt-get -y install ${PACKAGES}
+apt-get -o DPkg::Lock::Timeout=300 update -y
+apt-get -o DPkg::Lock::Timeout=300 install -y ${PACKAGES}
